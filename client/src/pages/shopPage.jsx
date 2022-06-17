c837f2bde721a5931638d37d98139bd96b5bd483
Alessandro Stella <alessandro.stella2004@gmail.com>
2022-06-17 13:45:22 +0200

    minor changes

--- client/src/pages/shopPage.jsx
+++ client/src/pages/shopPage.jsx
@@ -102,7 +102,7 @@ function shopPage() {
 
             <div className="shop-page">
                 <div className="select-category">
-                    <div className="title">AAAAAAAAAAA</div>
+                    <div className="title">Filter by category</div>
 
                     <div className="section">
                         {isSmall ? (
