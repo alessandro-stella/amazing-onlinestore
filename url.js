eef92d76cd7e5e23178ce2b4c5b2ca6a377c62d1
Alessandro Stella <alessandro.stella2004@gmail.com>
2023-05-20 18:59:01 +0200

    Changing url

--- url.js
+++ url.js
@@ -2,4 +2,4 @@ const isDeveloping = process.env.NODE_ENV == "development";
 
 export const url = isDeveloping
     ? "http://localhost:5000"
-    : "https://amazing-backend.vercel.app";
+    : "https://amazing-onlinestore.vercel.app";
